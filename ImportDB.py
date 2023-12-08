7e85cff9c5a486bd09f3e3e7c56f8b120bd58182
BenRWU <152934176+BenRWU@users.noreply.github.com>
2023-12-08 14:32:37 -0500

    Update ImportDB.py
    Re-did logic on importing DB for what we need only

--- ImportDB.py
+++ ImportDB.py
@@ -1,16 +1,11 @@
-pip install ucimlrepo
+dataSources = ("http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data","http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test")
 
-from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-adult = fetch_ucirepo(id=2) 
-  
-# data (as pandas dataframes) 
-X = adult.data.features 
-y = adult.data.targets 
-  
-# metadata 
-print(adult.metadata) 
-  
-# variable information 
-print(adult.variables) 
+def downloadSource(path='dataset', urls=dataSources):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    for url in urls:
+        response = requests.get(url)
+        name = os.path.basename(url)
+        with open(os.path.join(path, name), 'wb') as f:
+            f.write(response.content)
